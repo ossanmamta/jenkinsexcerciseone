2c3fd0f555562f43317d87500152f3ac40ed2788
ossanmamta <45501523+ossanmamta@users.noreply.github.com>
2019-08-28 16:01:45 +0100

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World this is mamta!";
+    private final String message = "Hello World this is!";
 
     public App() {}
 
